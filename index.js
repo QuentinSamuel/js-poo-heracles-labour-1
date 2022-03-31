ffb6e63d59c1a4c665c21840027b69325ca699cf
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-03-31 12:15:47 +0200

    js train with heracles-labour-1

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require('./src/Fighter.js')
+
+const heracles =  new Fighter( "🧔 Heracles", 20, 6);
+const lion = new Fighter("🦁 Nemean Lion", 11, 13);
+let round = 1
+
+while (heracles.life > 0 && lion.life > 0) {
+
+    heracles.fight(lion);
+    lion.fight(heracles);
+    round += 1;
+
+    
+    console.log(`🕛 Round #${round}`)
+    console.log(`${heracles.name} 🗡️  ${lion.name} 💙 ${lion.name}: ${lion.life}`) ;
+    console.log(`${lion.name} 🗡️  ${heracles.name} 💙 ${heracles.name}: ${heracles.life}`) ;
+}
+    console.log("")
+     
+if (heracles.life === 0) {
+    console.log (`💀 ${heracles.name} is dead ` )
+    console.log (`🏆 ${lion.name} wins (${lion.life} 💙 )`)
+}
+
+if (lion.life === 0){
+    console.log (`💀 ${lion.name} is dead ` )
+    console.log (`🏆 ${heracles.name} wins (${heracles.life} 💙 )`)
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,44 @@
 /* Fighter class definition */
+ 
+/*- a constant `MAX_LIFE = 100`: fighters have 100 max life points
+
+- a `Fighter` class with the properties:
+  - name: name of the fighter.
+  - strength: will be used to calculate the damage points during an attack.
+  - dexterity: will be used to calculate the defense points that will limit the damage received.
+  - life: initialized to MAX_LIFE, so they all start with 100 life points.
+
+There would potentially be plenty of other things to add, but that's a good start!*/
+
+
+const MAX_LIFE = 100
+
+function randomIt (min, max) {
+  return Math.floor(Math.random() * (max - min) + min); 
+}
+
+class Fighter{
+  constructor (name,strength,dexterity){
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(enemy) {
+    let damage = randomIt(1, this.strength) - enemy.dexterity;
+
+    if (damage < 0) {
+      damage = 0;
+    }
+
+    enemy.life = enemy.life - damage;
+
+    if (enemy.life < 0) {
+        enemy.life = 0;
+    }
+  }
+}
+
+module.exports = Fighter;
+
